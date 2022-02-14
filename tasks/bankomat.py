8041d26a5a2390d9ccc8d87f065319f9da5034f7
Turyan Azizov <turyanazizov13@gmail.com>
2022-02-14 20:06:44 +0400

    Correction

--- tasks/bankomat.py
+++ tasks/bankomat.py
@@ -1,27 +1,28 @@
-n=int(input("Cekilecek meblegi daxil edin: "))
+money=int(input("Cekilecek meblegi daxil edin: "))
 
-res=0
-while(n>0):
-    if n%10!=0 or n<=0:
-        print(-1)
-        break
-    elif n-500>=0:
-        n-=500
-        res+=1
-    elif n-200>=0:
-        n-=200
-        res+=1
-    elif n-100>=0:
-        n-=100
-        res+=1
-    elif n-50>=0:
-        n-=50
-        res+=1
-    elif n-20>=0:
-        n-=20
-        res+=1
-    elif n-10>=0:
-        n-=10
-        res+=1
+def fnc(n):
+    res = 0
+    while(n>0):
+        if n%10!=0 or n<=0:
+            return -1
+        elif n-500>=0:
+            n-=500
+            res+=1
+        elif n-200>=0:
+            n-=200
+            res+=1
+        elif n-100>=0:
+            n-=100
+            res+=1
+        elif n-50>=0:
+            n-=50
+            res+=1
+        elif n-20>=0:
+            n-=20
+            res+=1
+        elif n-10>=0:
+            n-=10
+            res+=1
+    return res
 
-print(res)
+print(fnc(money))
